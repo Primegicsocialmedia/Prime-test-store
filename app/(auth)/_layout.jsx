674898345956552de989950974b74c0664b1cbeb
Primegicsocialmedia <valentio.samuels777@gmail.com>
2024-11-08 13:43:29 +0200

    2nd commit

--- app/(auth)/_layout.jsx
+++ app/(auth)/_layout.jsx
@@ -1,12 +1,12 @@
 import { View, Text } from "react-native";
 import React from "react";
 
-const Home = () => {
+const AuthLayout = () => {
   return (
     <View>
-      <Text>Home</Text>
+      <Text>Auth Layout</Text>
     </View>
   );
 };
 
-export default Home;
+export default AuthLayout;
--- app/index.jsx
+++ app/index.jsx
@@ -1,9 +1,53 @@
-import { Text, View } from "react-native";
+import { StatusBar } from "expo-status-bar";
+import { ScrollView, StyleSheet, Text, View, Image } from "react-native";
+import { Link } from "expo-router";
+import { SafeAreaView } from "react-native-safe-area-context";
+import { images } from "../constants";
+import CustomButton from "../components/CustomButton";
+import { router } from "expo-router";
 
-export default function Index() {
+export default function App() {
   return (
-    <View className="flex-1 items-center justify-center bg-white">
-      <Text className="text-3xl font-bold">Hello World!</Text>
-    </View>
+    <SafeAreaView className="bg-primary h-full">
+      <ScrollView contentContainerStyle={{ height: "100%" }}>
+        <View className="w-full flex justify-center items-center min-h-[85vh] px-4">
+          <Image
+            source={images.logo}
+            className="w-[180px] h-[120px]"
+            resizeMode="contain"
+          />
+
+          <Image
+            source={images.cards}
+            className="max-w-[380px] w-full h-[300px] rounded-full"
+            resizeMode="contain"
+          />
+
+          <View className="relative mt-5">
+            <Text className="text-3xl text-white font-bold text-center">
+              Discover Endless{"\n"} Possibilities with{" "}
+              <Text className="text-secondary-200">Prime</Text>
+            </Text>
+
+            <Image
+              source={images.path}
+              className="w-[136px] h-[15px] absolute -bottom-2 -right-8"
+              resizeMode="contain"
+            />
+          </View>
+          <Text className="text-base font-pregular text-gray-100 mt-7 text-center">
+            Where Creativity Meets Innovation: Embark on a Journey of Limitless
+            Exploration with Prime
+          </Text>
+
+          <CustomButton
+            title="Continue to Home"
+            handlePress={() => router.push("/home")}
+            containerStyles="w-full mt-7"
+          />
+        </View>
+      </ScrollView>
+      <StatusBar backgroundColor="#161622" style="light" />
+    </SafeAreaView>
   );
 }
